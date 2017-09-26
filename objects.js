49ee6f2db74fbc497ce92ea7b96d1fec225e9f45
Sonny McPherson <unsungbomb@gmail.com>
2017-09-26 17:01:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ playlist[artistName] = songTitle;
   return playlist;
 }
 
-function removeFromPlaylist(playlist,artistName){
-delete playlist.artistName
- return playlist
+function removeFromPlaylist(playlist,artistName) {
+delete playlist[artistName];
+ return playlist;
  }
